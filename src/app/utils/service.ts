c380f384729ec809e0d2b962a3da8a3ccd1ecaa1
meryem-uludagli <meryemuslu2035@gmail.com>
2025-03-14 14:09:28 +0200

    add

--- src/app/utils/service.ts
+++ src/app/utils/service.ts
@@ -15,3 +15,32 @@ export const getCars = async (): Promise<GetCarsReturn> => {
 type GetCarReturn = {
   car: Car;
 };
+
+export const getCar = async (id: string): Promise<GetCarReturn> => {
+  const res = await fetch(`${API_URL}/api/cars/${id}`);
+
+  return res.json();
+};
+
+type GetPaymentUrlReturn = {
+  url: string;
+};
+
+export const getPaymentUrl = async (car: Car): Promise<GetPaymentUrlReturn> => {
+  const res = await fetch(`http://localhost:3000/api/checkout`, {
+    method: "POST",
+    body: JSON.stringify(car),
+  });
+
+  return res.json();
+};
+
+type GetOrdersReturn = {
+  orders: Order[];
+};
+
+export const getOrders = async (): Promise<GetOrdersReturn> => {
+  const res = await fetch(`http://localhost:3000/api/orders`);
+
+  return res.json();
+};

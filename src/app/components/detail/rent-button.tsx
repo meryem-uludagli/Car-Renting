164969574d27c218c7efaed28e7b4b69fdc3f31e
meryem-uludagli <meryemuslu2035@gmail.com>
2025-03-15 17:56:38 +0200

    add

--- src/app/components/detail/rent-button.tsx
+++ src/app/components/detail/rent-button.tsx
@@ -1,22 +1,35 @@
 "use client";
+
 import { Car } from "@/app/types";
 import { getPaymentUrl } from "@/app/utils/service";
+import { useState } from "react";
+import Loader from "../loader";
 
 interface Props {
   car: Car;
 }
 
 const RentButton = ({ car }: Props) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const handleRent = () => {
-    getPaymentUrl(car).then((data) => console.log(data));
+    setIsLoading(true);
+
+    getPaymentUrl(car)
+      .then((data) => {
+        window.location.href = data.url;
+      })
+      .finally(() => setIsLoading(false));
   };
+
   return (
     <button
       onClick={handleRent}
       className="bg-basic-blue py-2 px-6 text-white rounded-md hover:brightness-90 transition min-w-[150px]"
     >
-      Simdi Kirala
+      {isLoading ? <Loader size="size-5" /> : "Şimdi Kirala"}
     </button>
   );
 };
+
 export default RentButton;
